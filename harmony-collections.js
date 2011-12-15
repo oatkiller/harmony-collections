4f7a96fdf62d450b196246144617dfc620ac0a3d
Brandon Benvie <brandon@bbenvie.com>
2011-12-15 16:57:00 -0500

    Don't overwrite the native version

--- harmony-collections.js
+++ harmony-collections.js
@@ -1,5 +1,6 @@
-(function(exports){
+(function(exports, global){
   "use strict";
+  if (global.Map && global.Map.set && global.Map.get && global.Map.has && global.Map.delete) return;
 
   var Map = exports.Map = (function(){
     var maps = [], keysets = [], valsets = [], last = {};
@@ -11,6 +12,31 @@
       valsets.push([]);
       return map;
     }
+
+    Map.prototype = Object.create(null, {
+      set: value(function set(key, val){
+        var map = search(this, key);
+        if (map.index < 0) map.index = map.keys.length;
+        map.keys[map.index] = key;
+        last.keyi = map.index;
+        return map.vals[map.index] = val;
+      }),
+      get: value(function get(key){
+        var map = search(this, key);
+        return ~map.index ? map.vals[map.index] : undefined;
+      }),
+      has: value(function has(key){
+        return !!~search(this, key).index;
+      }),
+      delete: value(function del(key){
+        var map = search(this, key);
+        if (!~map.index) return false;
+        map.keys.splice(map.index, 1);
+        map.vals.splice(map.index, 1);
+        last.keyi = null;
+        return true;
+      })
+    });
 
     function search(map, key){
       var mapi = map === last.map ? last.mapi : find(maps, map);
@@ -39,34 +65,8 @@
       return -1;
     }
 
-    Map.prototype = Object.create(null, {
-      set: value(function set(key, val){
-        var map = search(this, key);
-        if (map.index < 0) map.index = map.keys.length;
-        map.keys[map.index] = key;
-        last.keyi = map.index;
-        return map.vals[map.index] = val;
-      }),
-      get: value(function get(key){
-        var map = search(this, key);
-        return ~map.index ? map.vals[map.index] : undefined;
-      }),
-      has: value(function has(key){
-        return !!~search(this, key).index;
-      }),
-      delete: value(function del(key){
-        var map = search(this, key);
-        if (!~map.index) return false;
-        map.keys.splice(map.index, 1);
-        map.vals.splice(map.index, 1);
-        last.keyi = null;
-        return true;
-      })
-    });
-
     return Object.freeze(Map);
   })();
-
 
   exports.WeakMap = (function(){
     var weakmaps = Map();
@@ -76,16 +76,6 @@
       var weakmap = Object.create(WeakMap.prototype);
       weakmaps.set(weakmap, Map());
       return weakmap;
-    }
-
-    function search(weakmap){
-      if (last.weakmap === weakmap) return last.map;
-      var map = weakmaps.get(weakmap);
-      if (~map) {
-        last = { weakmap: weakmap, map: map };
-        return map;
-      }
-      throw new TypeError("This isn't a WeakMap");
     }
 
     WeakMap.prototype = Object.create(null, {
@@ -103,6 +93,16 @@
       })
     });
 
+    function search(weakmap){
+      if (last.weakmap === weakmap) return last.map;
+      var map = weakmaps.get(weakmap);
+      if (~map) {
+        last = { weakmap: weakmap, map: map };
+        return map;
+      }
+      throw new TypeError("This isn't a WeakMap");
+    }
+
     return Object.freeze(WeakMap);
   })();
 
@@ -115,17 +115,6 @@
       sets.set(set, Map());
       return set;
     }
-
-    function search(set){
-      if (last.set === set) return last.map;
-      var map = sets.get(set);
-      if (~map) {
-        last = { set: set, map: map };
-        return map;
-      }
-      throw new TypeError("This isn't a Set");
-    }
-
     Set.prototype = Object.create(null, {
       has: value(function has(key){
         return search(this).has(key);
@@ -138,6 +127,16 @@
       })
     });
 
+    function search(set){
+      if (last.set === set) return last.map;
+      var map = sets.get(set);
+      if (~map) {
+        last = { set: set, map: map };
+        return map;
+      }
+      throw new TypeError("This isn't a Set");
+    }
+
     return Object.freeze(Set);
   })();
 
@@ -146,4 +145,7 @@
     return { value: val, enumerable: true };
   }
 
-})(typeof module === 'undefined' ? this : 'exports' in module ? module.exports : this);
+})(
+  typeof exports !== 'undefined' ? exports : this,
+  typeof global !== 'undefined' ? global : typeof window !== 'undefined' ? window : this
+);
