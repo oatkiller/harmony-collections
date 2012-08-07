b57d394241d502e70fd08c9f02805842e27d8e13
Brandon Benvie <brandon@bbenvie.com>
2012-08-06 21:32:34 -0400

    start adding some memory tests for WeakMap, fix tests for set returning undefined

--- test/weakmap-test.js
+++ test/weakmap-test.js
@@ -1,7 +1,8 @@
-var tap = require("tap");
+var tap = require('tap');
 var test = tap.test;
 var WM;
 var map;
+var memoryReading = require('./memory-reading');
 var secrets = ['secrets'];
 
 var methods = ['get', 'set', 'has', 'delete'];
@@ -24,9 +25,9 @@ test('basic usage', function(t){
   t.same(Object.getPrototypeOf(map), WM.prototype, 'instance of WeakMap.prototype');
   t.similar(Object.getOwnPropertyNames(map), [], 'no observable properties on the instance');
   t.same(map.get(WM), undefined, 'retreiving non-existant key returns undefined');
-  t.same(map.set(WM, secrets), secrets, 'set works and returns given val');
+  t.same(map.set(WM, secrets), undefined, 'set works and returns undefined');
   t.same(map.get(WM), secrets, 'retreiving works');
-  t.same(map.set(WM, 'overwrite'), 'overwrite', 'primitive value set works');
+  t.same(map.set(WM, 'overwrite'), undefined, 'primitive value set works');
   t.same(map.get(WM), 'overwrite', 'overwriting works');
   t.same(map.has(WM), true, 'has returns true');
   t.same(map.delete(WM), true, 'delete returns true');
@@ -44,74 +45,11 @@ test('errors', function(t){
 });
 
 
-function formatSize(s){
-  if (isNaN(s) || s <= 0) return '0b';
-  for (var b=0; s >= 1024; b++) s /= 1024;
-  return (b ? s.toFixed(2)+' '+' kmgt'[b] : s+' ')+'b';
-}
-
-function MemoryReading(name, time){
-  var reading = process.memoryUsage();
-  this.timing = process.hrtime(time);
-  this.name = name;
-  this.time = process.hrtime(zero)[1];
-  this.rss = reading.rss;
-  this.total = reading.heapTotal;
-  this.used = reading.heapUsed;
-}
-var zero = process.hrtime();
-
-MemoryReading.prototype = {
-  constructor: MemoryReading,
-  compare: function compare(other){
-    var first, last;
-    if (other.time > this.time) {
-      first = this;
-      last = other;
-    } else {
-      first = other;
-      last = this;
-    }
-    var out = Object.create(MemoryReading.prototype);
-    out.start = first;
-    out.end = last;
-    out.name = first.name + ' to ' + last.name;
-    out.time = last.time - first.time;
-    out.rss = last.rss - first.rss;
-    out.total = last.total - first.total;
-    out.used = last.total - first.total;
-    return out;
-  },
-  inspect: function(){
-    return require('util').inspect({
-      name: this.name,
-      time: this.time / 1000000 | 0,
-      rss: formatSize(this.rss),
-      total: formatSize(this.total),
-      used: formatSize(this.used)
-    });
-  }
-};
-
-var reading = MemoryReading.reading = function(readings){
-  return function reading(name){
-    name = name || 'auto';
-    if (name in readings) {
-      var result = readings[name].compare(new MemoryReading(name + '-end'));
-      delete readings[name];
-      return result;
-    } else {
-      readings[name] = new MemoryReading(name);
-    }
-  }
-}({});
-
-test('garbage collection', function(t){
-  reading();
-  for (var i=0; i < 100; i++)
-    new WM;
-  console.log(reading());
-  t.end();
-});
-
-// test('reif
+// test('garbage collection', function(t){
+//   memoryReading();
+//   for (var i=0; i < 100; i++) {
+//     new WM;
+//   }
+//   console.log(memoryReading());
+//   t.end();
+// });
